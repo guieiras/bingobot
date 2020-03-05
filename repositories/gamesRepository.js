31b323b3555808472e17ceed0ce0d2244420144d
Guilherme Eiras <guialbertoeiras@hotmail.com>
2020-03-04 22:59:56 -0300

    feat(games): add games repository

---
+++ repositories/gamesRepository.js
@@ -0,0 +1,16 @@
+const cryptoRandomString = require('crypto-random-string');
+
+module.exports = function GamesRepository(controller) {
+  async function createGame(user, team, channel) {
+    return controller.database.get('games').insert(
+      {
+        config: { user, team, channel },
+        createdAt: new Date(),
+        finishedAt: null,
+        signingKey: cryptoRandomString({ length: 16, type: 'url-safe' })
+      }
+    );
+  }
+
+  return { GamesRepository: { createGame } };
+}
