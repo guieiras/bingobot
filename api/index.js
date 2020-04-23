06e129612d032bc354bf5961c916819e46dd2b6c
Guilherme Eiras <guialbertoeiras@hotmail.com>
2020-04-23 11:49:50 -0300

    feat(draws): add multiple number support to one draw

--- api/index.js
+++ api/index.js
@@ -1,26 +1,34 @@
+const { numberWasDrawn } = require('../useCases/games/drawnNumbers');
+
 module.exports = function (controller) {
   controller.webserver.post('/games/:id/draws', async (req, res) => {
     const { id } = req.params;
     const signingKey = req.header('X-Signing-Key');
-    const number = parseInt(req.body && req.body.number);
+    const numbers = req.body.numbers &&
+      req.body.numbers.map((number) => parseInt(number));
 
     try {
-      if (isNaN(number)) {
-        return res.status(422).json({ error: { number: 'invalid' } });
+      if (numbers.filter((number) => isNaN(number)).length > 0) {
+        return res.status(422).json({ error: { numbers: 'invalid' } });
       }
 
       const game = await controller.repositories
         .GamesRepository
         .findGameByIdAndSigningKey(id, signingKey);
 
+      const newDraws = Array.from(new Set(numbers))
+        .filter((draw) => !numberWasDrawn(game, draw));
+
       if (game) {
         const bot = await controller.spawn(game.config.team);
         await bot.startConversationInChannel(game.config.channel);
-        bot.say(`<!here> ${number}`);
+        bot.say(`<!here> ${newDraws.join(', ')}`);
 
-        await controller.repositories
-          .GamesRepository
-          .addNumberToDrawn(game._id, number);
+        await Promise.all(newDraws.map((number) => (
+          controller.repositories
+            .GamesRepository
+            .addNumberToDrawn(game._id, number)
+        )));
 
         res.status(201).send('');
       } else {
